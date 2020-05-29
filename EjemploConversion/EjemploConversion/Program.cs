68aeea1943c016f368f5aedc08cdaab44017e121
Aurora <aurorajaldin2006@hotmail.com>
2020-05-29 18:51:41 -0300

    ejemplo de rehabilitacion

---
+++ EjemploConversion/EjemploConversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjemploConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
